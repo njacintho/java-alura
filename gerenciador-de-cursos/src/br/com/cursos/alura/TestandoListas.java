888a3952aa20759efc8745cb93d8adc9764a7fa9
Noemi <noemi.jacin@gmail.com>
2021-12-16 20:40:53 -0300

    Formas de percorrer a array

--- gerenciador-de-cursos/src/br/com/cursos/alura/TestandoListas.java
+++ gerenciador-de-cursos/src/br/com/cursos/alura/TestandoListas.java
@@ -1,6 +1,48 @@
 package br.com.cursos.alura;
 
-public class TestandoListas {
-	
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class TestandoListas {
+	public static void main(String[] args) {
+		String aula1 = "Conhecendo mais de listas";
+		String aula2 = "Modelando a classe Aula";
+		String aula3 = "Trabalhando com Cursos e Sets";
+
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+
+		System.out.println(aulas);
+
+		aulas.remove(0);
+
+		System.out.println(aulas);
+
+		for (String aula : aulas) {
+			System.out.println("Aula: " + aula);
+		}
+
+		String primeiraAula = aulas.get(0);
+		System.out.println("Aula: " + primeiraAula);
+
+		for (int i = 0; i < aulas.size(); i++) {
+			System.out.println("aula: " + aulas.get(i));
+		}
+
+		System.out.println(aulas.size());
+
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo ");
+			System.out.println("a aulas: " + aula);
+		});
+
+		aulas.add("Aumentando o nosso conehcimento");
+		System.out.println(aulas);
+		
+		Collections.sort(aulas);
+		System.out.println("Ordenado" + aulas);
+
+	}
 }
