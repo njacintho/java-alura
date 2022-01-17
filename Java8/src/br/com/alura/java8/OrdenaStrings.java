603fe038c854faaccb5a8d7de7beef350e817587
Noemi <noemi.jacin@gmail.com>
2022-01-17 11:56:47 -0300

    Default methods

---
+++ Java8/src/br/com/alura/java8/OrdenaStrings.java
@@ -0,0 +1,61 @@
+package br.com.alura.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("Alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("Caelum");
+		
+		// modo antigo para ordenar de outra forma.
+		Comparator<String> comparador = new ComparadorPorTamnaho();
+		
+		//Collections.sort(palavras, comparador);
+		palavras.sort(comparador);// não precisa ser no colletions 
+		System.out.println(palavras);
+		
+//		// foreach moo antigo
+//		for (String p : palavras) {
+//			System.out.println(p);
+//		}
+	
+		//nova maneira cosnumdor é uma nova de iterar 
+		Consumer<String> cosumidor = new imprimeNaLinha();
+		palavras.forEach(cosumidor);
+		
+		
+		
+	}
+
+}
+
+class imprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+	}
+	
+}
+
+
+class ComparadorPorTamnaho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+	
+}
